1e06bdd7abe30b7f43e75697dcb110fdf999c77d
NemanjaSt97 <49910899+NemanjaSt97@users.noreply.github.com>
2019-12-31 05:57:36 +0100

    Aplikacija

---
+++ Aplikacija/Aplikacija/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace CassandraWinFormsSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDodajdelatnost = new System.Windows.Forms.Button();
+            this.btnVratiDelatnost = new System.Windows.Forms.Button();
+            this.btnVratiDelatnosti = new System.Windows.Forms.Button();
+            this.btnObrisiDelatnost = new System.Windows.Forms.Button();
+            this.btnDodajKorisnika = new System.Windows.Forms.Button();
+            this.btnVratiKorisnika = new System.Windows.Forms.Button();
+            this.btnVratiKorisnike = new System.Windows.Forms.Button();
+            this.btnObrisiKorisnika = new System.Windows.Forms.Button();
+            this.btnObrisiTemu = new System.Windows.Forms.Button();
+            this.btnVratiTeme = new System.Windows.Forms.Button();
+            this.btnVratiTemu = new System.Windows.Forms.Button();
+            this.btnDodajTemu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDodajdelatnost
+            // 
+            this.btnDodajdelatnost.Location = new System.Drawing.Point(12, 26);
+            this.btnDodajdelatnost.Name = "btnDodajdelatnost";
+            this.btnDodajdelatnost.Size = new System.Drawing.Size(117, 23);
+            this.btnDodajdelatnost.TabIndex = 0;
+            this.btnDodajdelatnost.Text = "Dodaj delatnost";
+            this.btnDodajdelatnost.UseVisualStyleBackColor = true;
+            this.btnDodajdelatnost.Click += new System.EventHandler(this.btnDodajdelatnost_Click);
+            // 
+            // btnVratiDelatnost
+            // 
+            this.btnVratiDelatnost.Location = new System.Drawing.Point(12, 55);
+            this.btnVratiDelatnost.Name = "btnVratiDelatnost";
+            this.btnVratiDelatnost.Size = new System.Drawing.Size(117, 23);
+            this.btnVratiDelatnost.TabIndex = 1;
+            this.btnVratiDelatnost.Text = "Vrati delatnost";
+            this.btnVratiDelatnost.UseVisualStyleBackColor = true;
+            this.btnVratiDelatnost.Click += new System.EventHandler(this.btnVratiDelatnost_Click);
+            // 
+            // btnVratiDelatnosti
+            // 
+            this.btnVratiDelatnosti.Location = new System.Drawing.Point(12, 84);
+            this.btnVratiDelatnosti.Name = "btnVratiDelatnosti";
+            this.btnVratiDelatnosti.Size = new System.Drawing.Size(117, 23);
+            this.btnVratiDelatnosti.TabIndex = 2;
+            this.btnVratiDelatnosti.Text = "Vrati delatnosti";
+            this.btnVratiDelatnosti.UseVisualStyleBackColor = true;
+            this.btnVratiDelatnosti.Click += new System.EventHandler(this.btnVratiDelatnosti_Click);
+            // 
+            // btnObrisiDelatnost
+            // 
+            this.btnObrisiDelatnost.Location = new System.Drawing.Point(12, 113);
+            this.btnObrisiDelatnost.Name = "btnObrisiDelatnost";
+            this.btnObrisiDelatnost.Size = new System.Drawing.Size(117, 23);
+            this.btnObrisiDelatnost.TabIndex = 3;
+            this.btnObrisiDelatnost.Text = "Obrisi delatnost";
+            this.btnObrisiDelatnost.UseVisualStyleBackColor = true;
+            this.btnObrisiDelatnost.Click += new System.EventHandler(this.btnObrisiDelatnost_Click);
+            // 
+            // btnDodajKorisnika
+            // 
+            this.btnDodajKorisnika.Location = new System.Drawing.Point(188, 26);
+            this.btnDodajKorisnika.Name = "btnDodajKorisnika";
+            this.btnDodajKorisnika.Size = new System.Drawing.Size(117, 23);
+            this.btnDodajKorisnika.TabIndex = 4;
+            this.btnDodajKorisnika.Text = "Dodaj korisnika";
+            this.btnDodajKorisnika.UseVisualStyleBackColor = true;
+            this.btnDodajKorisnika.Click += new System.EventHandler(this.btnDodajKorisnika_Click);
+            // 
+            // btnVratiKorisnika
+            // 
+            this.btnVratiKorisnika.Location = new System.Drawing.Point(188, 55);
+            this.btnVratiKorisnika.Name = "btnVratiKorisnika";
+            this.btnVratiKorisnika.Size = new System.Drawing.Size(117, 23);
+            this.btnVratiKorisnika.TabIndex = 5;
+            this.btnVratiKorisnika.Text = "Vrati korisnika";
+            this.btnVratiKorisnika.UseVisualStyleBackColor = true;
+            this.btnVratiKorisnika.Click += new System.EventHandler(this.btnVratiKorisnika_Click);
+            // 
+            // btnVratiKorisnike
+            // 
+            this.btnVratiKorisnike.Location = new System.Drawing.Point(188, 84);
+            this.btnVratiKorisnike.Name = "btnVratiKorisnike";
+            this.btnVratiKorisnike.Size = new System.Drawing.Size(117, 23);
+            this.btnVratiKorisnike.TabIndex = 6;
+            this.btnVratiKorisnike.Text = "Vrati korisnike";
+            this.btnVratiKorisnike.UseVisualStyleBackColor = true;
+            this.btnVratiKorisnike.Click += new System.EventHandler(this.btnVratiKorisnike_Click);
+            // 
+            // btnObrisiKorisnika
+            // 
+            this.btnObrisiKorisnika.Location = new System.Drawing.Point(188, 113);
+            this.btnObrisiKorisnika.Name = "btnObrisiKorisnika";
+            this.btnObrisiKorisnika.Size = new System.Drawing.Size(117, 23);
+            this.btnObrisiKorisnika.TabIndex = 7;
+            this.btnObrisiKorisnika.Text = "Obrisi korisnika";
+            this.btnObrisiKorisnika.UseVisualStyleBackColor = true;
+            this.btnObrisiKorisnika.Click += new System.EventHandler(this.btnObrisiKorisnika_Click);
+            // 
+            // btnObrisiTemu
+            // 
+            this.btnObrisiTemu.Location = new System.Drawing.Point(357, 113);
+            this.btnObrisiTemu.Name = "btnObrisiTemu";
+            this.btnObrisiTemu.Size = new System.Drawing.Size(117, 23);
+            this.btnObrisiTemu.TabIndex = 11;
+            this.btnObrisiTemu.Text = "Obrisi temu";
+            this.btnObrisiTemu.UseVisualStyleBackColor = true;
+            this.btnObrisiTemu.Click += new System.EventHandler(this.btnObrisiTemu_Click);
+            // 
+            // btnVratiTeme
+            // 
+            this.btnVratiTeme.Location = new System.Drawing.Point(357, 84);
+            this.btnVratiTeme.Name = "btnVratiTeme";
+            this.btnVratiTeme.Size = new System.Drawing.Size(117, 23);
+            this.btnVratiTeme.TabIndex = 10;
+            this.btnVratiTeme.Text = "Vrati teme";
+            this.btnVratiTeme.UseVisualStyleBackColor = true;
+            this.btnVratiTeme.Click += new System.EventHandler(this.btnVratiTeme_Click);
+            // 
+            // btnVratiTemu
+            // 
+            this.btnVratiTemu.Location = new System.Drawing.Point(357, 55);
+            this.btnVratiTemu.Name = "btnVratiTemu";
+            this.btnVratiTemu.Size = new System.Drawing.Size(117, 23);
+            this.btnVratiTemu.TabIndex = 9;
+            this.btnVratiTemu.Text = "Vrati temu";
+            this.btnVratiTemu.UseVisualStyleBackColor = true;
+            this.btnVratiTemu.Click += new System.EventHandler(this.btnVratiTemu_Click);
+            // 
+            // btnDodajTemu
+            // 
+            this.btnDodajTemu.Location = new System.Drawing.Point(357, 26);
+            this.btnDodajTemu.Name = "btnDodajTemu";
+            this.btnDodajTemu.Size = new System.Drawing.Size(117, 23);
+            this.btnDodajTemu.TabIndex = 8;
+            this.btnDodajTemu.Text = "Dodaj temu";
+            this.btnDodajTemu.UseVisualStyleBackColor = true;
+            this.btnDodajTemu.Click += new System.EventHandler(this.btnDodajTemu_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 332);
+            this.Controls.Add(this.btnObrisiTemu);
+            this.Controls.Add(this.btnVratiTeme);
+            this.Controls.Add(this.btnVratiTemu);
+            this.Controls.Add(this.btnDodajTemu);
+            this.Controls.Add(this.btnObrisiKorisnika);
+            this.Controls.Add(this.btnVratiKorisnike);
+            this.Controls.Add(this.btnVratiKorisnika);
+            this.Controls.Add(this.btnDodajKorisnika);
+            this.Controls.Add(this.btnObrisiDelatnost);
+            this.Controls.Add(this.btnVratiDelatnosti);
+            this.Controls.Add(this.btnVratiDelatnost);
+            this.Controls.Add(this.btnDodajdelatnost);
+            this.Name = "Form1";
+            this.Text = "Cassandra Win Forms Sample";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDodajdelatnost;
+        private System.Windows.Forms.Button btnVratiDelatnost;
+        private System.Windows.Forms.Button btnVratiDelatnosti;
+        private System.Windows.Forms.Button btnObrisiDelatnost;
+        private System.Windows.Forms.Button btnDodajKorisnika;
+        private System.Windows.Forms.Button btnVratiKorisnika;
+        private System.Windows.Forms.Button btnVratiKorisnike;
+        private System.Windows.Forms.Button btnObrisiKorisnika;
+        private System.Windows.Forms.Button btnObrisiTemu;
+        private System.Windows.Forms.Button btnVratiTeme;
+        private System.Windows.Forms.Button btnVratiTemu;
+        private System.Windows.Forms.Button btnDodajTemu;
+
+
+    }
+}
+
