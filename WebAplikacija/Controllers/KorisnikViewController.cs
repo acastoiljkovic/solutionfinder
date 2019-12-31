0db34f21aa475fde33ddfef0508b460784747a29
Aleksandar Stoiljkovic <aca.stoiljkovic@elfak.rs>
2019-12-31 17:24:23 +0100

    Dodata Web Aplikacija

---
+++ WebAplikacija/Controllers/KorisnikViewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAplikacija.Controllers
+{
+    public class KorisnikViewController : Controller
+    {
+        // GET: KorisnikView
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("korisnikview/korisnik/{id}")]
+        public ActionResult Korisnik(string id)
+        {
+            return View(CassandraDataLayer.DataProvider.VratiKorisnika(id));
+        }
+    }
+}
---
+++ WebAplikacija/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAplikacija.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(CassandraDataLayer.QueryEntities.Korisnik korisnik)
+        {
+            korisnik.korisnikID = Guid.NewGuid().ToString("N");
+            if (CassandraDataLayer.DataProvider.DodajKorisnika(korisnik))
+                return View(korisnik);
+            else
+                return View();
+        }
+    }
+}
