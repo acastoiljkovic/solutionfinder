4e115458477559bbe70f47f43ed1f601173a1e16
Aleksandar Stoiljkovic <aca.stoiljkovic@elfak.rs>
2020-01-03 16:12:44 +0100

    Postovi oddavanje i prikaz na Home page

---
+++ WebAplikacija/Controllers/PostController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAplikacija.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(CassandraDataLayer.QueryEntities.Tema tema)
+        {
+            tema.korisnikID = CassandraDataLayer.DataStore.GetInstance().GetKorisnik().korisnikID;
+            tema.temaID = Guid.NewGuid().ToString("N");
+            if (CassandraDataLayer.DataProvider.DodajTemu(tema))
+                return Redirect("~/Home");
+            return View();
+        }
+    }
+}
